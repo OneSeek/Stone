5aade6cc09fa46dc0c3cc3307e65122f2ee42fd8
OneSeek <1668207735@qq.com>
2020-01-19 23:15:28 +0800

    代码清单7.10 FuncInterpreter.java

---
+++ src/chap7/FuncInterpreter.java
@@ -0,0 +1,11 @@
+// 代码清单7.10 FuncInterpreter.java
+package chap7;
+import stone.FuncParser;
+import stone.ParseException;
+import chap6.BasicInterpreter;
+
+public class FuncInterpreter extends BasicInterpreter {
+	public static void main(String[] args) throws ParseException {
+		run(new FuncParser(),new NestedEnv());
+	}
+}
