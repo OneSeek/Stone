1cb13680e4dc54d95a7e454298e4979695794223
OneSeek <1668207735@qq.com>
2020-01-21 21:59:26 +0800

    代码清单9.7 ClassInfo.java

---
+++ src/chap9/ClassInfo.java
@@ -0,0 +1,44 @@
+// 代码清单9.7 ClassInfo.java
+package chap9;
+import stone.StoneException;
+import stone.ast.ClassBody;
+import stone.ast.ClassStmnt;
+import chap6.Environment;
+
+public class ClassInfo {
+	protected ClassStmnt definition;
+	protected Environment environment;
+	protected ClassInfo superClass;
+
+	public ClassInfo(ClassStmnt cs, Environment env) {
+		definition = cs;
+		environment = env;
+		Object obj = env.get(cs.superClass());
+		if (obj == null)
+			superClass = null;
+		else if (obj instanceof ClassInfo)
+			superClass = (ClassInfo) obj;
+		else
+			throw new StoneException("unkonw super class: " + cs.superClass(), cs);
+	}
+	
+	public String name() {
+		return definition.name();
+	}
+	
+	public ClassInfo superClass() {
+		return superClass;
+	}
+	
+	public ClassBody body() {
+		return definition.body();
+	}
+	
+	public Environment environment() {
+		return environment;
+	}
+	
+	public String toString() {
+		return "<class " + name() + ">";
+	}
+}
