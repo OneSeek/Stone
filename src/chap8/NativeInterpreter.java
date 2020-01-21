8f667ba8df67f5741b8ec46ae11466eb2037d01d
OneSeek <1668207735@qq.com>
2020-01-21 21:52:33 +0800

    代码清单8.4 NativeInterpreter.java

---
+++ src/chap8/NativeInterpreter.java
@@ -0,0 +1,12 @@
+// 代码清单8.4 NativeInterpreter.java
+package chap8;
+import stone.ClosureParser;
+import stone.ParseException;
+import chap6.BasicInterpreter;
+import chap7.NestedEnv;
+
+public class NativeInterpreter extends BasicInterpreter {
+	public static void main(String[] args) throws ParseException {
+		run(new ClosureParser(),new Natives().environment(new NestedEnv()));
+	}
+}
